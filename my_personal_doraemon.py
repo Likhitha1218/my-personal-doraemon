45a7479d3ccd2bb848adce27dd55ca9df3af7b84
Likhitha1218 <150022438+Likhitha1218@users.noreply.github.com>
2025-07-19 17:34:55 +0530

    Update my_personal_doraemon.py

--- my_personal_doraemon.py
+++ my_personal_doraemon.py
@@ -14,7 +14,7 @@ st.markdown(f'''
     @import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
 
     .stApp {{
-        background-image: url("https://static.vecteezy.com/system/resources/previews/003494732/hand-drawn-cute-bunny-hearts-seamless-pattern.jpg");
+        background-image: url("https://www.rawpixel.com/image/19266965/soft-pink-hearts-pattern");
         background-repeat: repeat;
         background-size: auto;
         background-attachment: fixed;
