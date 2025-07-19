482f5c0d5b8d5f3ac9c286e39a06d1dc9941e2b8
Likhitha1218 <150022438+Likhitha1218@users.noreply.github.com>
2025-07-19 17:02:49 +0530

    Update my_personal_doraemon.py

--- my_personal_doraemon.py
+++ my_personal_doraemon.py
@@ -10,7 +10,8 @@ st.markdown('''
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
     .stApp {
-        background-image: url("https://www.transparenttextures.com/patterns/paper-fibers.png");
+        background-image: url("https://i.imgur.com/tZcGdoJ.png");
+
         background-size: cover;
         background-repeat: no-repeat;
         background-attachment: fixed;
